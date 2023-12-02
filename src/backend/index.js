9dcc747a18788b823e3ee228a0fea95bc0c64ce8
Steven Beallis <beallis3@illinois.edu>
2023-12-02 17:11:09 -0600

    server update

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,63 +8,78 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// let pool;
+let pool;
 
-// const createTcpPool = async () => {
-//   const dbConfig = {
-//     host: '34.66.177.166',
-//     port: '3306',
-//     user: 'root',
-//     password: 'teamGreen',
-//   };
+const createTcpPool = async () => {
+  const dbConfig = {
+    host: '34.66.177.166',
+    port: '3306',
+    user: 'root',
+    password: 'teamGreen',
+  };
 
-//   return mysql.createPool(dbConfig);
-// };
+  return mysql.createPool(dbConfig);
+};
 
-// const initializePool = async () => {
-//   try {
-//     pool = await createTcpPool();
-//     console.log("Connected to Cloud SQL");
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
+const initializePool = async () => {
+  try {
+    pool = await createTcpPool();
+    console.log("Connected to Cloud SQL");
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-// initializePool();
+initializePool();
 
-var connection = mysql.createConnection({
-  host: "34.66.177.166",
-  port: "3306",
-  user: "root",
-  password: "teamGreen",
-  database: "steam_game_data"
-});
+// var connection = mysql.createConnection({
+//   host: "34.66.177.166",
+//   port: "3306",
+//   user: "root",
+//   password: "teamGreen",
+//   database: "steam_game_data"
+// });
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT * FROM gameInfo limit 100;', function(err, rows, fields) {
-  if(err) console.log(err);
-  console.log('The solution is: ', rows);
-  connection.end();
-});
+// connection.query('SELECT * FROM gameInfo limit 100;', function(err, rows, fields) {
+//   if(err) console.log(err);
+//   console.log('The solution is: ', rows);
+//   connection.end();
+// });
 
 
 app.get("/api/searchGames", async (req, res) => {
   try {
-    var genreId = 4;
-    var platformId = 2;
-    var categoryId = 6;
+    // var genreId = 4;
+    // var platformId = 2;
+    // var categoryId = 6;
 
-    const sqlSelect =
-      "SELECT * FROM steam_game_data.gameInfo WHERE genreId = " +
-      genreId +
-      " AND platformId = " +
-      platformId +
-      " AND categoryId = " +
-      categoryId;
+    // const sqlSelect =
+    //   "SELECT * FROM steam_game_data.gameInfo WHERE genreId = " +
+    //   genreId +
+    //   " AND platformId = " +
+    //   platformId +
+    //   " AND categoryId = " +
+    //   categoryId;
 
     // const rows = await pool.query(sqlSelect);
     // res.json(rows);
+
+    // Simple SELECT query
+    const query = "SELECT * FROM steam_game_data.gameInfo limit 100;";
+        
+    // Execute the query
+    pool.query(query, (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error executing query");
+      } else {
+        // Send the query results as the response
+        res.json(results);
+      }
+    });
+
   } catch (error) {
     console.error("Error handling the GET request:", error);
     res.status(500).send("Server Error: " + error);
