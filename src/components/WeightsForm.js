c44c95ac584c6162e32bdb489ee04371b25f2be5
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2023-12-03 18:29:39 -0600

    updated weights form

--- src/components/WeightsForm.js
+++ src/components/WeightsForm.js
@@ -1,63 +1,29 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-//can do onChange={} inside input for the range
-
-const submitWeights = async () => {
-//   // Convert state objects to JSON strings
-//   const priceWeightString = JSON.stringify(priceWeight);
-//   const metacriticWeightString = JSON.stringify(metacriticWeight);
-//   const recommendationCountWeightString = JSON.stringify(recommendationCountWeight);
-//   const steamSpyPlayerWeightString = JSON.stringify(steamSpyPlayerWeight);
-
-//   // Make the GET request with query parameters
-//   axios
-//     .get("http://localhost:3002/api/searchGames", {
-//       params: {
-//         genres: genresString,
-//         platforms: platformsString,
-//         categories: categoriesString,
-//         selectValues: selectValuesString,
-//         searchBarValues: searchBarValueString,
-//       },
-//     })
-//     .then(async (response) => {
-//       // setFilteredGamesTemp(response.data);
-//       var temp = response.data;
-//       if (userName !== " ") {
-//         var scoredGames = await weightMath(temp);
-//         console.log("TEST: " + scoredGames);
-//         setFilteredGames(scoredGames);
-//         console.log(filteredGames);
-//       } else {
-//         setFilteredGames(temp);
-//       }
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       alert("Error: " + error.message); // Handle the error appropriately
-//     });
- };
-
-
 const WeightsForm = () => {
-  const [priceWeight, setPriceWeight] = useState(0);
-  const [metacriticWeight, setMetacriticWeight] = useState(0);
-  const [recommendationCountWeight, setRecommendationCountWeight] = useState(0);
-  const [steamSpyPlayerWeight, setsteamSpyPlayerWeight] = useState(0);
+  const [priceWeight, setPriceWeight] = useState(5); // Default to 5
+  const [metacriticWeight, setMetacriticWeight] = useState(5); // Default to 5
+  const [recommendationCountWeight, setRecommendationCountWeight] = useState(5); // Default to 5
+  const [steamSpyPlayerWeight, setSteamSpyPlayerWeight] = useState(5); // Default to 5
 
   const handlePriceChange = (event) => {
-    setPriceWeight(parseInt(event.target.value));
+    const value = parseInt(event.target.value);
+    setPriceWeight(Math.min(Math.max(value, 1), 10)); // Ensure the value is between 1 and 10
   };
   const handleMetacriticChange = (event) => {
-    setMetacriticWeight(parseInt(event.target.value));
+    const value = parseInt(event.target.value);
+    setMetacriticWeight(Math.min(Math.max(value, 1), 10));
   };
   const handleRecommendationChange = (event) => {
-    setRecommendationCountWeight(parseInt(event.target.value));
+    const value = parseInt(event.target.value);
+    setRecommendationCountWeight(Math.min(Math.max(value, 1), 10));
   };
   const handleSpyPlayersChange = (event) => {
-    setsteamSpyPlayerWeight(parseInt(event.target.value));
+    const value = parseInt(event.target.value);
+    setSteamSpyPlayerWeight(Math.min(Math.max(value, 1), 10));
   };
+
   return (
     <div className="container-fluid mt-5">
       <div className="row justify-content-center">
@@ -67,53 +33,66 @@ const WeightsForm = () => {
               <h4>Rank Profile</h4>
             </div>
             <div className="card-body">
-              <label htmlFor="customRangePrice" className="form-label">
-                How much does the price of the game impact your choice?
-              </label>
-              <input
-                type="range"
-                className="form-range"
-                step="10"
-                id="customRangePrice"
-                onChange={handlePriceChange}
-              />
-              <span>{priceWeight}</span>
-              <label htmlFor="customRangeMetacritic" className="form-label">
-                How much does the metacritic score of the game impact your choice?
-              </label>
-              <input
-                type="range"
-                className="form-range"
-                step="10"
-                id="customRangeMetacritic"
-                onChange={handleMetacriticChange}
-              />
-              <span>{metacriticWeight}</span>
-              <label htmlFor="customRangeRecommendation" className="form-label">
-                How much does the number of recommendations of the game impact your choice?
-              </label>
-              <input
-                type="range"
-                className="form-range"
-                step="10"
-                id="customRangeRecommendation"
-                onChange={handleRecommendationChange}
-              />
-              <span>{recommendationCountWeight}</span>
-              <label htmlFor="customRangeSteamSpy" className="form-label">
-                How much does the steam spy players estimate of the game impact your choice?
-              </label>
-              <input
-                type="range"
-                className="form-range"
-                step="10"
-                id="customRangeSteamSpy"
-                onChange={handleSpyPlayersChange}
-              />
-              <span>{steamSpyPlayerWeight}</span>
-              <h6>
-                0 means it doesn't impact your choice at all and 10 means it is very important to you
-              </h6>
+              <div className="mt-4">
+                <h4 htmlFor="customRangePrice" className="form-label text-center">
+                  How much does the price of the game impact your choice?
+                </h4>
+                <input
+                  type="range"
+                  className="form-range"
+                  step="1"
+                  min="1"
+                  max="10"
+                  id="customRangePrice"
+                  onChange={handlePriceChange}
+                />
+                <h4 className="text-center">{priceWeight}</h4>
+              </div>
+              <div className="mt-4">
+                <h4 htmlFor="customRangeMetacritic" className="form-label text-center">
+                  How much does the metacritic score of the game impact your choice?
+                </h4>
+                <input
+                  type="range"
+                  className="form-range"
+                  step="1"
+                  min="1"
+                  max="10"
+                  id="customRangeMetacritic"
+                  onChange={handleMetacriticChange}
+                />
+                <h4 className="text-center">{metacriticWeight}</h4>
+              </div>
+              <div className="mt-4">
+                <h4 htmlFor="customRangeRecommendation" className="form-label text-center">
+                  How much does the number of recommendations of the game impact your choice?
+                </h4>
+                <input
+                  type="range"
+                  className="form-range"
+                  step="1"
+                  min="1"
+                  max="10"
+                  id="customRangeRecommendation"
+                  onChange={handleRecommendationChange}
+                />
+                <h4 className="text-center">{recommendationCountWeight}</h4>
+              </div>
+              <div className="mt-4">
+                <h4 htmlFor="customRangeSteamSpy" className="form-label text-center">
+                  How much does the steam spy players estimate of the game impact your choice?
+                </h4>
+                <input
+                  type="range"
+                  className="form-range"
+                  step="1"
+                  min="1"
+                  max="10"
+                  id="customRangeSteamSpy"
+                  onChange={handleSpyPlayersChange}
+                />
+                <h4 className="text-center">{steamSpyPlayerWeight}</h4>
+              </div>
             </div>
           </div>
         </div>
